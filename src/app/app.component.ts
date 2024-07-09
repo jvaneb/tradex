b9151b3a585e643aab6d4893629deb226134d383
jvaneb <jbautistavallejo@gmail.com>
2024-07-08 20:05:07 -0500

    Template V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'finance_website-temp';
+  title = 'finance_website';
 }
