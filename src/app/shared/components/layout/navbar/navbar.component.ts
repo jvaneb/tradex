6159ea6edc5dbe7d6c1adf117c808b6096d59d3c
jvaneb <jbautistavallejo@gmail.com>
2024-07-10 16:52:36 -0500

    Template V3

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, AfterViewInit, HostListener  } from '@angular/core';
 import * as bootstrap from 'bootstrap';
 
 @Component({
@@ -9,8 +9,28 @@ import * as bootstrap from 'bootstrap';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements AfterViewInit {
+
+  ngOnInit(): void {
+    this.onScroll();
+  }
+
   ngAfterViewInit() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = Array.from(tooltipTriggerList).map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.onScroll();
+  }
+
+  private onScroll(): void {
+    const navbar = document.getElementById('navbar');
+    if (navbar) {      
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const action = scrollTop > navbar.offsetHeight ? 'add' : 'remove';
+      navbar.classList[action]('scrolled');
+      navbar.classList[action === 'add' ? 'remove' : 'add']('transparent');
+    }
   }
 }
