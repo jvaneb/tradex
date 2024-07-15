9bd50d020a636f9878608ee2b5d8abd79af3f315
jvaneb <jbautistavallejo@gmail.com>
2024-07-15 17:00:18 -0500

    Template ajuste V5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../shared/components/carousel/carousel.component';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CarouselComponent],
+  imports: [CommonModule, CarouselComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -19,5 +20,50 @@ export class HomeComponent {
     { url: '../../assets/img/currency-tesla.png', alt: 'Descripción de la imagen 2', title: 'Título de la imagen 2' },
     { url: '../../assets/img/currency-apple.png', alt: 'Descripción de la imagen 3', title: 'Título de la imagen 3' },
     { url: '../../assets/img/currency-gold.png', alt: 'Descripción de la imagen 3', title: 'Título de la imagen 3' },
+  ];
+
+  cards = [
+    {
+      title: 'Highly regulated & licensed',
+      text: 'Authorized and regulated by multintional globally leading regulatory bodies',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    },
+    {
+      title: 'Card title 2',
+      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    },
+    {
+      title: 'Highly regulated & licensed',
+      text: 'Authorized and regulated by multintional globally leading regulatory bodies',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    },
+    {
+      title: 'Card title 2',
+      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    },
+    {
+      title: 'Highly regulated & licensed',
+      text: 'Authorized and regulated by multintional globally leading regulatory bodies',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    },
+    {
+      title: 'Card title 2',
+      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      links: [
+        { text: 'Learn More', href: '#' }
+      ]
+    }
   ];
 }
---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() text: string = '';
+  @Input() links: { text: string, href: string }[] = [];
+}
